b9ed73ca000fd7ce61ca67a522271ec10585b523
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-03 19:03:24 -0300

    Add files via upload

---
+++ combustiveis_fosseis_democracia_autocracia.R
@@ -0,0 +1,17 @@
+
+# Combustíveis fósseis em países autocratas e democratas -----------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 03/12/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/fossil-fuels --------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Pela maior parte da história da humanidade nossos ancestrais dependeram de muitas formas
+### básicas de energia: força humana e animal, e queima de biomassa de madeira e cultivos.
+### Mas a Revolução Industrial desbloqueou uma nova fonte de energia: combustíveis fósseis.
+### Energia a partir de combustíveis fósseis tem sido um fundamental condutor social, econômico
+### tecnológico e responsável pelo progesso do desenvolvimento que seguiu.
+
+###
+
+
